a2baba005657631635d6eeb2bcf6216c69a209ed
Jamesmalo <9jmalo@stpatrick.org>
2017-08-28 14:43:31 -0500

    Add files via upload

---
+++ Vehicle.js
@@ -0,0 +1,16 @@
+let Vehicle = function(e,f,g,ampg,m){
+  let engine = e;
+  let fuel = f;
+  let gastank = g;
+  let averageMPG = ampg;
+  let milesDriven = m;
+
+  function totalMiles(){
+
+  }
+
+  function fuelLeft(){
+
+  }
+
+}
