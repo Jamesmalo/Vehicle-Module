3a559fd75987b384977274f99051811366a5b5e1
Jamesmalo <9jmalo@stpatrick.org>
2017-08-29 14:40:05 -0500

    day 2

--- Vehicle.js
+++ Vehicle.js
@@ -1,8 +1,8 @@
-let Vehicle = function(e,f,g,ampg,m){
+let Vehicle = function(e,f,g,avg,m){
   let engine = e;
   let fuel = f;
   let gastank = g;
-  let averageMPG = ampg;
+  let averageMPG = avg;
   let milesDriven = m;
 
   function totalMiles(){
@@ -13,4 +13,37 @@ let Vehicle = function(e,f,g,ampg,m){
 
   }
 
+  function fill(p){
+    let 
+  }
+
+  function drive(h){
+    milesDriven += (h*avg);
+
+    fuel -= (m*h)
+
+    if(f == 0){
+      console.log("You are out of fuel");
+      return f == 0;
+    }
+    else if(f > 0){
+      console.log("You still have fuel");
+    }
+  }
+
+  function engineEfficiency(){
+    if(e == 4){
+      return 1;
+    }
+    if else(e == 6){
+      return 1.2;
+    }
+    if else(e == 8){
+      return 1.36;
+    }
+    else{
+      return console.log("null");
+    }
+  }
+  return{totalMiles,fuelLeft,fill,drive}
 }
